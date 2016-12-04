e97c7a097fe708e1943bc7c34f3affceb593265f
studente007 <stshinglol@gmail.com>
2016-12-04 23:28:58 +0100

    getting tired of this

--- js/tag.js
+++ js/tag.js
@@ -1,32 +1,7 @@
-function myFunction1() {
-    var x = document.getElementById('myDIV');
+function myFact() {
+    var x = document.getElementById('myFacten');
     if (x.style.display === 'none') {
         x.style.display = 'block';
     } else {
         x.style.display = 'none';
     }
-}
-function myFunction2() {
-    var x = document.getElementById('myDIV2');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-function myFunction3() {
-    var x = document.getElementById('myDIV3');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-function myFunction4() {
-    var x = document.getElementById('myDIV4');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
