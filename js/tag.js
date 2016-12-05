714bfe0ac495508a4500051e40f9085005990e2c
studente007 <stshinglol@gmail.com>
2016-12-05 02:27:28 +0100

    give up and made a new idea

--- js/tag.js
+++
@@ -1,7 +0,0 @@
-function myFact() {
-    var x = document.getElementById('myFacten');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
